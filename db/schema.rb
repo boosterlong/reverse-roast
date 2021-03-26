1aaf2cbea61056b867956e0a6f19fd0c78cff238
boosterlong <chartrandblake93@gmail.com>
2021-03-25 22:34:57 -0700

    Fixed new prompt text box

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_03_18_201625) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "prompt"
     t.datetime "created_at", precision: 6, null: false
